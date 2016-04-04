f230e30385004d5f5c078d7dc8e573092b03914c
Eric Olszewski <eolszewski@gmail.com>
2016-04-04 00:46:40 -0500

    Remove unnecessary files and use real dialogs.

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 var restify = require('restify');
 var builder = require('botbuilder');
+var index = require('../dialogs/index')
 
 // Create bot and add dialogs
-var bot = new builder.BotConnectorBot({ appId: 'ericTestBot', appSecret: '42b2ae35b43b4e38ab48aecc0f59287e' });
-bot.add('/', function (session) {
-    session.send("I'm worthless, but now we don't have to buy pizza :)");
+var bot = new builder.BotConnectorBot({ 
+    appId: "ericTestBot", 
+    appSecret: "42b2ae35b43b4e38ab48aecc0f59287e" 
 });
+bot.add('/', index);
 
 // Setup Restify Server
 var server = restify.createServer();
 server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
 server.listen(process.env.port || 3978, function () {
-    console.log('%s listening to %s', server.name, server.url);
+   console.log('%s listening to %s', server.name, server.url); 
 });
